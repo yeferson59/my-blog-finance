11eff42f506ae9f4868a421b1426fe5b0e7041ec
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-10-02 00:52:53 -0500

    add enpoints de authentication with credential and sign out session

---
+++ src/pages/api/auth/login.ts
@@ -0,0 +1,57 @@
+import type { APIContext } from "astro";
+import { verify } from "@node-rs/argon2";
+import { db, lucia } from "@/auth";
+import { z } from "astro:schema";
+
+const signInSchema = z.object({
+  email: z.string().email().trim(),
+  password: z.string().min(8).trim()
+})
+
+const userSchema = z.object({
+  id: z.string(),
+  email: z.string().email().optional(),
+  passwordHash: z.string().optional()
+})
+
+export async function POST(context: APIContext): Promise<Response> {
+  const formData = await context.request.formData()
+  const { success, error, data } = await signInSchema.safeParseAsync({
+    email: formData.get('email'),
+    password: formData.get('password')
+  })
+  if (!success) return Response.json({ errors: error.flatten().fieldErrors }, { status: 400 })
+
+  const { rows } = await db.execute({
+    sql: "SELECT * FROM USER WHERE EMAIL = ?;",
+    args: [data.email]
+  })
+
+  if (!rows[0]) return Response.json({ message: "Invalid credentials" }, { status: 400 })
+
+  const user = await userSchema.parseAsync({
+    id: rows[0].id,
+    email: rows[0].email,
+    passwordHash: rows[0].password_hash
+  })
+
+  if (!user.passwordHash) return Response.json({ message: "" })
+
+  const validPassword = await verify(user.passwordHash, data.password, {
+    memoryCost: 19456,
+    timeCost: 2,
+    outputLen: 32,
+    parallelism: 1
+  });
+  if (!validPassword) {
+    return Response.json("Incorrect email or password", {
+      status: 400
+    });
+  }
+
+  const session = await lucia.createSession(user.id, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
+
+  return context.redirect("/")
+}
