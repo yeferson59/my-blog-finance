c83d909d123d9eeaf37c2929b34665016f6f8656
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-10-02 00:54:50 -0500

    add components of shadcn

---
+++ src/components/ui/icons.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import type { LucideProps } from "lucide-react"
+import {
+  Moon,
+  SunMedium,
+  Twitter,
+  Github,
+  // Nota: 'Google' no está disponible, usaremos 'Mail' como alternativa
+  Mail,
+  LogOut,
+  LogIn,
+  ArrowRight
+} from "lucide-react"
+
+export type Icon = React.ComponentType<LucideProps>
+
+export const Icons = {
+  sun: SunMedium,
+  moon: Moon,
+  twitter: Twitter,
+  gitHub: Github,
+  google: Mail, // Usamos Mail como alternativa para Google
+  logOut: LogOut,
+  logIn: LogIn,
+  arrowRight: ArrowRight
+}
