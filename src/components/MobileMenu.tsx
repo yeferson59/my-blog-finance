f9279dc50151cc2a3f3b1045a611a0a05ac93351
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-10-04 01:29:52 -0500

    channge name loyout and routing

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -33,7 +33,7 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ isAuthenticated }) => {
               <a className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400" href="/#inicio">Inicio</a>
               <a className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400" href="/#tecnologia">Tecnología</a>
               <a className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400" href="/#finanzas">Finanzas</a>
-              <a className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400" href="/sobre-nosotros">Sobre Nosotros</a>
+              <a className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400" href="/about">Sobre Nosotros</a>
               <a className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400" href="/blog">Posts</a>
               <div className="pt-4 pb-10">
                 {isAuthenticated ? (
