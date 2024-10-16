a864fef3da12f4e50cb187c054d64bda2f6ba08e
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-10-16 13:04:33 -0500

    improve view of modebutton

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import * as React from "react";
 import { Moon, Sun } from "lucide-react";
-import { Button } from "@/components/ui/button";
 
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
