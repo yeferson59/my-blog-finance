bdf46514fc2aa21dec88a366d64cc9749536937b
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-10-16 12:03:09 -0500

    improve way of get article

--- src/pages/api/comment.ts
+++ src/pages/api/comment.ts
@@ -28,21 +28,21 @@ export async function POST(context: APIContext): Promise<Response> {
     );
 
   try {
-    const articlesDocument = await sql(
-      "SELECT * FROM ARTICLES WHERE DOCUMENT_ID = $1;",
+    const [articlesDocument] = await sql(
+      "SELECT * FROM ARTICLES WHERE DOCUMENT_ID = $1 AND PUBLISHED_AT IS NULL;",
       [data.documentId]
     );
 
     await sql(
-      "INSERT INTO COMMENT(POST_ID, USER_ID, CONTENT) VALUES($1, $2, $3)",
-      [articlesDocument[0].id, data.userId, data.content]
+      "INSERT INTO COMMENT(POST_ID, USER_ID, CONTENT) VALUES($1, $2, $3);",
+      [articlesDocument.id, data.userId, data.content]
     );
 
-    const rows = await sql(
+    const [rows] = await sql(
       "SELECT COMMENT.*, AUTH_USER.username, AUTH_USER.email, AUTH_USER.avatar FROM COMMENT JOIN AUTH_USER ON COMMENT.user_id = AUTH_USER.id WHERE COMMENT.post_id = $1;",
-      [articlesDocument[0].id]
+      [articlesDocument.id]
     );
-    return Response.json(rows[0], { status: 200 });
+    return Response.json(rows, { status: 200 });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
--- src/pages/api/comments/[articleId].ts
+++ src/pages/api/comments/[articleId].ts
@@ -19,16 +19,18 @@ export async function GET(context: APIContext): Promise<Response> {
       { status: 400 }
     );
 
-  const articleData = await sql("SELECT * FROM ARTICLES WHERE ID = $1", [data]);
+  const articleData = await sql("SELECT * FROM ARTICLES WHERE ID = $1;", [
+    data,
+  ]);
 
-  const articlesDocument = await sql(
-    "SELECT * FROM ARTICLES WHERE DOCUMENT_ID = $1",
+  const [articlesDocument] = await sql(
+    "SELECT * FROM ARTICLES WHERE DOCUMENT_ID = $1 AND PUBLISHED_AT IS NULL;",
     [articleData[0].document_id]
   );
 
   const rows = await sql(
     "SELECT COMMENT.*, AUTH_USER.username, AUTH_USER.email, AUTH_USER.avatar FROM COMMENT JOIN AUTH_USER ON COMMENT.user_id = AUTH_USER.id WHERE COMMENT.post_id = $1;",
-    [articlesDocument[0].id]
+    [articlesDocument.id]
   );
 
   return Response.json(rows);
