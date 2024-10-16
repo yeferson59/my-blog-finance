531e9d4c28641291e3e5aec836c7711eb326a9a4
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-10-16 12:51:38 -0500

    improve modeToggle

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
+import React from "react";
 import { Moon, Sun } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
