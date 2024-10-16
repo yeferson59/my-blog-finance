344e2246e373de541bde5cae9e5d9c6eeeca1c50
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-10-16 12:03:34 -0500

    add config of title and seo

---
+++ src/utils/config.ts
@@ -0,0 +1,10 @@
+const WEBSITE_DATA = {
+  siteMetadataTitle:
+    "Tecnofinanzas: dónde la innovación se encuentra con la economía",
+  siteMetadataDescription:
+    "Mantente a la vanguardia de la tecnología y las finanzas con las publicaciones de tecnofinanzas.",
+  siteMetadataHeaderTitle: "TecnoFinanzas",
+  siteMetadataLenguaje: "es-la",
+  websiteUrl: new URL("https://tecnofinanzas.blog"),
+};
+export default WEBSITE_DATA;
