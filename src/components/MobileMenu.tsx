47b35d8ced5b6bf7b49af7254e2f08a9cf98dec8
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-10-20 14:40:19 -0500

    improve view of header in mode mobile and desktop

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,10 +1,11 @@
 // src/components/MobileMenu.tsx
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/ui/icons";
-import SignOut from './SignOut';
-import Newsletter from './Newsletter';
-import type { User } from 'lucia';
+import SignOut from "./SignOut";
+import Newsletter from "./Newsletter";
+import { ModeToggle } from "./ModeToggle";
+import type { User } from "lucia";
 
 interface MobileMenuProps {
   isAuthenticated: User | null;
@@ -24,18 +25,47 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ isAuthenticated }) => {
         <div className="fixed inset-0 z-50 bg-white dark:bg-gray-800 overflow-y-auto">
           <div className="container mx-auto px-4 py-6">
             <div className="flex justify-end">
-              <Button variant="outline" size="icon" onClick={() => setIsOpen(false)}>
+              <Button
+                variant="outline"
+                size="icon"
+                onClick={() => setIsOpen(false)}
+              >
                 <Icons.x className="h-6 w-6" />
                 <span className="sr-only">Cerrar menú</span>
               </Button>
             </div>
             <nav className="flex flex-col space-y-4 mt-6">
-              <a className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400" href="/#inicio">Inicio</a>
-              <a className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400" href="/#tecnologia">Tecnología</a>
-              <a className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400" href="/#finanzas">Finanzas</a>
-              <a className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400" href="/about">Sobre Nosotros</a>
-              <a className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400" href="/blog">Posts</a>
-              <div className="pt-4 pb-10">
+              <a
+                className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400 transition-colors duration-200"
+                href="/#inicio"
+              >
+                Inicio
+              </a>
+              <a
+                className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400 transition-colors duration-200"
+                href="/#tecnologia"
+              >
+                Tecnología
+              </a>
+              <a
+                className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400 transition-colors duration-200"
+                href="/#finanzas"
+              >
+                Finanzas
+              </a>
+              <a
+                className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400 transition-colors duration-200"
+                href="/about"
+              >
+                Sobre Nosotros
+              </a>
+              <a
+                className="text-lg font-medium text-gray-700 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400 transition-colors duration-200"
+                href="/blog"
+              >
+                Posts
+              </a>
+              <div className="pt-4 pb-6">
                 {isAuthenticated ? (
                   <SignOut />
                 ) : (
@@ -47,8 +77,9 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ isAuthenticated }) => {
                   </a>
                 )}
               </div>
-              <div className=" w-full flex justify-end items-center">
+              <div className="flex flex-col space-y-4">
                 <Newsletter />
+                <ModeToggle />
               </div>
             </nav>
           </div>
