87b532fc0d967b059497426e39ea6498c74325c9
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-10-24 00:26:08 -0500

    remove method get

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -19,22 +19,3 @@ export async function POST(context: APIContext): Promise<Response> {
 
   return context.redirect("/auth/signin");
 }
-
-export async function GET(context: APIContext): Promise<Response> {
-  if (!context.locals.session) {
-    return new Response(null, {
-      status: 401,
-    });
-  }
-
-  await lucia.invalidateSession(context.locals.session.id);
-
-  const sessionCookie = lucia.createBlankSessionCookie();
-  context.cookies.set(
-    sessionCookie.name,
-    sessionCookie.value,
-    sessionCookie.attributes,
-  );
-
-  return context.redirect("/auth/signin");
-}
