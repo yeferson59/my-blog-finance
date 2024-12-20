72ea0962996f0708bb6a5940f8a75d0a95416e66
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-12-19 20:32:43 -0500

    add get posts of endpoint

---
+++ src/pages/api/posts.ts
@@ -0,0 +1,59 @@
+import type Post from "@/interfaces/post";
+import fetchApi from "@/lib/strapi";
+import type { APIContext } from "astro";
+
+import { redisUrl } from "@/config/env.config";
+import { createClient } from "redis";
+
+// Configura Redis con conexión reutilizable
+const client = createClient({
+  url: redisUrl,
+});
+
+// Asegúrate de que Redis esté conectado antes de usarlo
+if (!client.isOpen) {
+  client
+    .connect()
+    .catch((err) => console.error("Error al conectar con Redis:", err));
+}
+
+export async function GET(context: APIContext): Promise<Response> {
+  let allPosts: Post[] = [];
+
+  try {
+    // Intenta obtener los datos desde Redis
+    const data = await client.get("allPosts");
+
+    if (!data) {
+      // Si no hay datos en Redis, consulta la API
+      allPosts = await fetchApi<Post[]>({
+        endpoint:
+          "articles?fields[0]=title&fields[1]=description&fields[2]=pubDate&fields[3]=featured&fields[4]=slug&populate[cover][fields]=formats&populate[category][fields]=name,slug",
+        wrappedByKey: "data",
+        method: "GET",
+        query: {
+          "pagination[page]": 1,
+          "pagination[pageSize]": 6,
+          sort: "pubDate:desc",
+        },
+      });
+
+      // Guarda los datos en Redis con expiración de 12 horas
+      await client.set("allPosts", JSON.stringify(allPosts), {
+        EX: 60 * 60 * 12, // 12 horas
+      });
+
+      return Response.json(allPosts, { status: 200 });
+    }
+
+    // Si hay datos en Redis, parsea y devuélvelos
+    allPosts = JSON.parse(data);
+    return Response.json(allPosts, { status: 200 });
+  } catch (error) {
+    console.error("Error al obtener los artículos:", error);
+    return Response.json(
+      { message: "Error interno al obtener los artículos." },
+      { status: 500 },
+    );
+  }
+}
--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,6 +1,1 @@
-import { redisUrl } from "@/config/env.config";
-import { createClient } from "redis";
 
-export const client = createClient({
-  url: redisUrl,
-});
