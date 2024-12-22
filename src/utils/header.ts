6436a701366dcce37d6bd6882b189e3161f72071
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-12-22 00:01:51 -0500

    add list of title and link of header

---
+++ src/utils/header.ts
@@ -0,0 +1,22 @@
+export const MENU = [
+  {
+    name: "Inicio",
+    href: "/",
+  },
+  {
+    name: "Tecnología",
+    href: "/#tecnologia",
+  },
+  {
+    name: "Finanzas",
+    href: "/#finanzas",
+  },
+  {
+    name: "Sobre mi",
+    href: "/about",
+  },
+  {
+    name: "Posts",
+    href: "/blog",
+  },
+];
