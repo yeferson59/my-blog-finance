85d2998118a8aaafe4cf630640cdd41350e700c8
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-05-02 20:20:14 -0500

    feat: Switch to Node adapter and Tailwind Vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,28 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 import WEBSITE_DATA from "./src/utils/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import node from "@astrojs/node";
 
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+  integrations: [react()],
+
   site: WEBSITE_DATA.siteUrl,
+
   security: {
     checkOrigin: true,
   },
+
   output: "server",
-  adapter: netlify({
-    imageCDN: false,
-    cacheOnDemandPages: true,
+
+  adapter: node({
+    mode: "standalone",
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
