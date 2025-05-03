6a86608d3ed60b09a26c47ca1b751bf167959fa8
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-05-03 00:14:46 -0500

    Switch from Node.js to Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,16 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import WEBSITE_DATA from "./src/utils/config";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   integrations: [react()],
-
   site: WEBSITE_DATA.siteUrl,
-
   output: "server",
-
-  adapter: node({
-    mode: "standalone",
+  adapter: netlify({
+    imageCDN: true,
   }),
-
   vite: {
     plugins: [tailwindcss()],
-    server: {
-      allowedHosts: ["my-blog-frontend-sunt1c6ktw.155.133.22.54.sslip.io"],
-    },
   },
 });
