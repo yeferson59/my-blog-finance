0534782e5dc2dfce7e34aaff879744f013404d62
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-05-02 20:25:14 -0500

    Remove Netlify and Node integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import netlify from "@astrojs/netlify";
 import WEBSITE_DATA from "./src/utils/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import node from "@astrojs/node";
 
 export default defineConfig({
